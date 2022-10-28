fb0c49ae62541a6dfcba87b8e105543cdcdaa1fd
msiebrandeis <87743045+msiebrandeis@users.noreply.github.com>
2022-10-28 11:37:38 -0500

    Delete upload.sh

--- upload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#store in /home/ec2-user
-aws s3 rm s3://rseg176 --recursive
-
-aws s3 sync /var/www/html/wordpress/wp-content/uploads/2022/10/ s3://rseg176 --exclude='*' --include='emails.csv'
